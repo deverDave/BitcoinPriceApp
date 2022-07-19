5f4b9465bdfc940ca667efe4df402814d497d048
David Jeppson <djeppson@summitslc.com>
2022-07-19 10:13:55 -0600

    publicPath config update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: '/',
+      publicPath: process.env.NODE_ENV === "production" ? "/BitcoinPriceApp/" : "/",
     filename: 'build.js'
   },
   module: {
