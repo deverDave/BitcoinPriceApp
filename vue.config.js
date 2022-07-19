39369eae7e837c17760bdc75bf8ffa0bd062ce36
David Jeppson <djeppson@summitslc.com>
2022-07-19 08:13:22 -0600

    change path for pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/BitcoinPriceApp/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/BitcoinPriceApp/" : "/dist/",
 };
